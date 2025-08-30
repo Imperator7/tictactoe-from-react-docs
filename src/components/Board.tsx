0159254d7e3baa9efe9cd319a748b49003fd42b3
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-08-30 10:01:44 +0700

    Already timed out for 1 hour after this commit is to build till it finished

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,6 @@
 import type { Player } from '../App'
 import Square from './Square'
-import { type Dispatch, type SetStateAction } from 'react'
+import { useState, type Dispatch, type SetStateAction } from 'react'
 
 type BoardProps = {
   turn: Player
@@ -8,14 +8,24 @@ type BoardProps = {
 }
 
 export default function Board({ turn, setTurn }: BoardProps) {
-  console.log(turn)
+  const [marks, setMarks] = useState<Player[]>([
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ])
 
   return (
     <>
       {[0, 1, 2].map((r) => (
         <div key={r} className="board-row">
           {[0, 1, 2].map((box) => (
-            <Square key={`${r} - ${box}`} turn={turn} setTurn={setTurn} />
+            <Square key={`${r * 3 + box + 1}`} turn={turn} setTurn={setTurn} />
           ))}
         </div>
       ))}
