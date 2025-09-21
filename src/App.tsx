8c2d3e009767d05e18fe47c82536d26556341c9f
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-09-21 22:59:57 +0700

    remove Lobby unfinished component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Game from './components/Game'
-import Lobby from './components/Lobby'
 import './App.css'
 
 function App() {
   return (
     <div className="p-16 min-h-screen flex flex-col items-center  bg-lime-50">
       <Game />
-      <Lobby />
     </div>
   )
 }
