161a5a970336a5954fdbef27c5f31fbd0777b8f4
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-09-21 22:53:21 +0700

    add margin on top of the board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Game from './components/Game'
+import Lobby from './components/Lobby'
 import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-center  bg-lime-50">
+    <div className="p-16 min-h-screen flex flex-col items-center  bg-lime-50">
       <Game />
+      <Lobby />
     </div>
   )
 }
