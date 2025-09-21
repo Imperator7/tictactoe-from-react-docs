d1ab3bf653be9a3686c1721bf8be098e4c06d3eb
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-09-21 23:54:56 +0700

    fix mobile ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="p-16 min-h-screen flex flex-col items-center  bg-lime-50">
+    <div className="py-32 min-h-screen flex flex-col items-center  bg-lime-50">
       <Game />
     </div>
   )
