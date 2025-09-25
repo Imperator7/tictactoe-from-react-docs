6bb254057d3fe5f08f958b4a1d016d5ea02b8fd0
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-09-25 15:53:55 +0700

    make the background to black cover

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,32 @@ import './App.css'
 
 function App() {
   return (
-    <div className="max-h-dvh relative overflow-hidden inset-0 overscroll-none bg-black">
-      <picture className="absolute inset-0 bg-cover">
-        <source type="image/avif" srcSet="/space.avif" sizes="100vh" />
-        <source type="image/webp" srcSet="/space.webp" sizes="100vh" />
-        <img
-          src="/space.jpg"
-          alt="space-background"
-          className="absolute inset-0 w-full h-full bg-black"
-          loading="eager"
-          fetchPriority="high"
-          sizes="100vw"
-        />
-      </picture>
-      <div className="absolute z-10 top-[50%] left-[50%] w-1 h-1 bg-white rounded-full animate-[shoot_1.5s_linear_infinite]" />
-      <div className="absolute z-10 top-[35%] left-[70%] w-1 h-1 bg-white rounded-full animate-[shoot_3s_linear_infinite]" />
-      <div className="absolute z-10 top-[25%] left-[20%] w-1 h-1 bg-white rounded-full animate-[shoot_1.5s_linear_infinite]" />
-      <div className="absolute z-10 top-[20%] left-[50%] w-1 h-1 bg-white rounded-full animate-[shoot_1s_linear_infinite]" />
-      <div className="absolute z-10 top-[65%] left-[90%] w-1 h-1 bg-white rounded-full animate-[shoot_1s_linear_infinite]" />
-      <div className="absolute z-10 top-[40%] left-[50%] w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]" />
-      <div className="absolute z-10 top-[40%] left-[33%] w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]" />
-      <div className="absolute z-10 top-[40%] left-[5%]  w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]" />
-      <div className="absolute z-10 top-[70%] left-[8%]  w-1 h-1 bg-white rounded-full animate-[shootInterval_3s_linear_infinite]" />
-      <div className="py-32 relative min-h-screen flex flex-col items-center ">
-        <Game />
+    <div className="bg-black bg-cover">
+      <div className="max-h-dvh relative overflow-hidden inset-0 overscroll-none bg-black">
+        <picture className="absolute inset-0 bg-cover">
+          <source type="image/avif" srcSet="/space.avif" sizes="100vh" />
+          <source type="image/webp" srcSet="/space.webp" sizes="100vh" />
+          <img
+            src="/space.jpg"
+            alt="space-background"
+            className="absolute inset-0 w-full h-full bg-black"
+            loading="eager"
+            fetchPriority="high"
+            sizes="100vw"
+          />
+        </picture>
+        <div className="absolute z-10 top-[50%] left-[50%] w-1 h-1 bg-white rounded-full animate-[shoot_1.5s_linear_infinite]" />
+        <div className="absolute z-10 top-[35%] left-[70%] w-1 h-1 bg-white rounded-full animate-[shoot_3s_linear_infinite]" />
+        <div className="absolute z-10 top-[25%] left-[20%] w-1 h-1 bg-white rounded-full animate-[shoot_1.5s_linear_infinite]" />
+        <div className="absolute z-10 top-[20%] left-[50%] w-1 h-1 bg-white rounded-full animate-[shoot_1s_linear_infinite]" />
+        <div className="absolute z-10 top-[65%] left-[90%] w-1 h-1 bg-white rounded-full animate-[shoot_1s_linear_infinite]" />
+        <div className="absolute z-10 top-[40%] left-[50%] w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]" />
+        <div className="absolute z-10 top-[40%] left-[33%] w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]" />
+        <div className="absolute z-10 top-[40%] left-[5%]  w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]" />
+        <div className="absolute z-10 top-[70%] left-[8%]  w-1 h-1 bg-white rounded-full animate-[shootInterval_3s_linear_infinite]" />
+        <div className="py-32 relative min-h-screen flex flex-col items-center ">
+          <Game />
+        </div>
       </div>
     </div>
   )
