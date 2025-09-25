31f77a679f1fb63a7f62cd733670baf6e1b79cd1
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-09-25 13:51:06 +0700

    make the shooting star effect responsively with the screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen relative">
-      <div className="z-10 absolute top-50 left-50 w-1 h-1 bg-white rounded-full animate-[shoot_1.5s_linear_infinite]"></div>
-      <div className="z-50 absolute top-170 left-110 w-1 h-1 bg-white rounded-full animate-[shoot_3=s_linear_infinite]"></div>
-      <div className="z-50 absolute top-25 left-200 w-1 h-1 bg-white rounded-full animate-[shoot_1.5s_linear_infinite]"></div>
-      <div className="z-50 absolute top-20 left-50 w-1 h-1 bg-white rounded-full animate-[shoot_1s_linear_infinite]"></div>
-      <div className="z-50 absolute top-150 left-300 w-1 h-1 bg-white rounded-full animate-[shoot_1s_linear_infinite]"></div>
-      <div className="z-50 absolute top-40 left-150 w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]"></div>
-      <div className="z-50 absolute top-40 left-100 w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]"></div>
-      <div className="z-50 absolute top-40 left-10 w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]"></div>
-      <div className="z-50 absolute top-100 left-15 w-1 h-1 bg-white rounded-full animate-[shootInterval_3s_linear_infinite]"></div>
+    <div className="min-h-[100dvh] relative overflow-hidden inset-0">
+      <div className="absolute z-10 top-[50%] left-[50%] w-1 h-1 bg-white rounded-full animate-[shoot_1.5s_linear_infinite]" />
+      <div className="absolute z-10 top-[35%] left-[70%] w-1 h-1 bg-white rounded-full animate-[shoot_3s_linear_infinite]" />
+      <div className="absolute z-10 top-[25%] left-[20%] w-1 h-1 bg-white rounded-full animate-[shoot_1.5s_linear_infinite]" />
+      <div className="absolute z-10 top-[20%] left-[50%] w-1 h-1 bg-white rounded-full animate-[shoot_1s_linear_infinite]" />
+      <div className="absolute z-10 top-[65%] left-[90%] w-1 h-1 bg-white rounded-full animate-[shoot_1s_linear_infinite]" />
+      <div className="absolute z-10 top-[40%] left-[50%] w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]" />
+      <div className="absolute z-10 top-[40%] left-[33%] w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]" />
+      <div className="absolute z-10 top-[40%] left-[5%]  w-1 h-1 bg-white rounded-full animate-[shootInterval_4s_linear_infinite]" />
+      <div className="absolute z-10 top-[70%] left-[8%]  w-1 h-1 bg-white rounded-full animate-[shootInterval_3s_linear_infinite]" />
       <img src="/space.jpg" alt="" className="absolute min-h-screen" />
       <div className="py-32 relative min-h-screen flex flex-col items-center">
         <Game />
